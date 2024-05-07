5f6b7803de34565f03db57a156873c47ab02f984
hansangho51 <dhdh8236@daum.net>
2024-05-07 20:37:50 +0900

    최종테스트입니다.

--- src/App.js
+++ src/App.js
@@ -5,23 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          테스트중입니다.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          두번째 테스트 중입니다.
-          세번째 테스트 중입니다.
-          해치웠나?
-          진짜 오늘 찐막 테스트
-        </a>
+        테스트 중입니다.
       </header>
     </div>
   );
